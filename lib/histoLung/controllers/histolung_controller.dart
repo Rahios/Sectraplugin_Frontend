de31c777d04d9b49c55cafead89297ca50bbca83
elias <borrajo.elias@gmail.com>
2024-08-02 01:19:55 +0200

    Fix communication backend & frontend.

--- lib/histoLung/controllers/histolung_controller.dart
+++ lib/histoLung/controllers/histolung_controller.dart
@@ -24,15 +24,26 @@ class HistolungController
         headers: {'Content-Type': 'application/json'},
         body: json.encode({'ImageName': imageName}), //Same 'ImageName' param name as the API expects in the request body
       ) // Set a timeout for the request
-          .timeout(const Duration(minutes: 3, seconds: 40),
+          .timeout(const Duration(minutes: 4),
           onTimeout: () {
             String message = 'Connection timed out, operation took too long';
             print(message);
             throw Exception(message);
           });
 
+
       print('Response status: ${response.statusCode}');
 
+      // wait some more time before trying again to retrieve the result
+      if (response.statusCode == 404) 
+      {
+        // ToDo : The timeout is not respected : This is a temporary solution to handle the timeout issue.
+        //  the request is not interrupted after 4 minutes. It breaks sooner with a 404 error.
+        // So the temproray solution is to retry after 40 seconds and then every 10 seconds for 3 minutes.
+        // The final solution is to make a POST that will send the operation to the backend.
+        // A wait to wait that the operation is finished and then a GET to retrieve the result.
+        return await retrieveDelayed();
+      }
 
       if (response.statusCode == 200)
       {
@@ -50,6 +61,39 @@ class HistolungController
       print('Error analyzing image: $e');
       throw Exception('Failed to analyze image');
     }
+  }
+
+  Future<HistolungModel> retrieveDelayed() async {
+    print('Image not found, waiting 40 seconds before retrying');
+    
+    await Future.delayed(const Duration(seconds: 40));
+    
+    // Initialize a timer to retry for 3 minutes
+    int elapsedSeconds = 0;
+    const int retryInterval = 10;
+    const int maxRetries = 18; // 3 minutes (180 seconds) / 10 seconds per retry
+    
+    // Retry every 10 seconds until the max number of retries is reached or the image is found
+    while (elapsedSeconds < maxRetries * retryInterval)
+    {
+      await Future.delayed(const Duration(seconds: retryInterval));
+      try
+      {
+        HistolungModel latestAnalysis = await getLastAnalysis();
+        if (latestAnalysis.prediction.isNotEmpty)
+        {
+          return latestAnalysis;
+        }
+      }
+      catch (e)
+      {
+        print('Retrying... Elapsed time: $elapsedSeconds seconds');
+      }
+    
+      elapsedSeconds += retryInterval;
+    }
+    
+    throw Exception('Failed to retrieve image after multiple retries');
   }
 
 
@@ -130,12 +174,12 @@ class HistolungController
         print('Images list retrieved successfully');
         print('Images list: $imageList');
         return imageList;
-      } 
+      }
       else {
         throw Exception('Failed to scan images folder. Status code not == 200');
       }
-    } 
-    catch (e) 
+    }
+    catch (e)
     {
       print('Error scanning images folder: $e');
       throw Exception('Failed to scan images folder');
